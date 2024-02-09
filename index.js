23275218eac65ff8e831353e6a045c841a8b28ef
kanyaw <kanyawyousif243@gmail.com>
2024-02-09 19:16:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(startingNumber) {
+    let count = startingNumber;
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  // Example usage:
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const messages = writeCards(names, eventName);
+  console.log(messages);
+  
+  countDown(10);
+  
